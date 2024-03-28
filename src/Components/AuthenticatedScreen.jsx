7af944da0fa452c771bdbef77cada7d3a7b43bd3
Alice Georgina <alicewilson1@Alices-MBP.lan>
2024-03-28 10:16:57 +0000

    started styling home page

--- src/Components/AuthenticatedScreen.jsx
+++ src/Components/AuthenticatedScreen.jsx
@@ -1,24 +1,55 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet, ImageBackground, StatusBar, SafeAreaView} from "react-native";
 import { styles } from "../../App";
+import colours from "../styles/colours";
 import { useNavigation } from '@react-navigation/native'
+import { collectManifestSchemes } from "expo-linking";
 
 
 
 const AuthenticatedScreen = ({ user, handleAuthentication }) => {
     const navigation = useNavigation()
-    console.log(navigation, 'navigation')
 
     return (
       
 
-      <View style={styles.authContainer}>
-        <Text style={styles.title}>Welcome</Text>
-        <Text style={styles.emailText}></Text>
+      <View style={{flex: 1}}>
+        <ImageBackground source={require('../styles/images/mountain.jpg')}
+style={{ flex: 1, width: '100%', height: '100%'}}>
+  <View style={style.details}>
+    <Text style={{color: colours.white, fontSize:35, fontWeight: 'bold'}}>The trip of a lifetime awaits.</Text>
+        <Text style={{color: colours.white, lineHeight: 25, marginTop: 15, fontSize:16}}>Take the hassle out of holiday planning and let our TravelBot do the heavy lifting for you.</Text>
+  
+  <View style={style.btn}>
+    <Text style={{fontWeight: 'bold'}}>Get Started</Text>
+  </View>
+  </View>
+
         <Button title='Start your journey planning!' onPress={() => navigation.navigate('ChatBot')}></Button>
         <Button title="Logout" onPress={handleAuthentication} color="#e74c3c" />
+      </ImageBackground>
       </View>
       
     );
   };
+
+const style= StyleSheet.create({
+  details:{
+    height: '70%',
+    bottom: 0,
+    position: 'absolute',
+    paddingHorizontal: 40,
+  },
+  btn: {
+    height: 50,
+    width: 120,
+    backgroundColor: colours.white,
+    marginTop: 20,
+    borderRadius: 7,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+
 
   export default AuthenticatedScreen
