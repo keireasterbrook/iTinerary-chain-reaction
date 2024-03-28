487d812aaae726489f3ebaf5aa2ac4d602825143
Alice Georgina <alicewilson1@Alices-MBP.lan>
2024-03-28 09:56:59 +0000

    added a styling folder

---
+++ src/styles/colours.js
@@ -0,0 +1,10 @@
+const colours = {
+    white: '#FFF',
+    black: '#000',
+    darkpurple: '#5E376D',
+    lightpurple: '#BD97CB',
+    cream: '#F3EAC0',
+    darkgrey: '#747080'
+}
+
+export default colours
